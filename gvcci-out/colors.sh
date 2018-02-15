f8c73305942f84f089d65d6924c16db412db7562
Victor Veytsman <vityavv@gmail.com>
2018-02-15 17:45:42 -0500

    Add raw GVCCI output

---
+++ gvcci-out/colors.sh
@@ -0,0 +1,23 @@
+
+# Special
+background='#21120e'
+foreground='#fbe5a4'
+cursor='#fbe5a4'
+
+# Colors
+color0='#45251e'
+color1='#f34023'
+color2='#4ea04c'
+color3='#9ebb41'
+color4='#47b17f'
+color5='#e3800d'
+color6='#32ac97'
+color7='#f7c9ba'
+color8='#69382d'
+color9='#fa705a'
+color10='#6fbc6e'
+color11='#b7ce6a'
+color12='#6fc79e'
+color13='#f59e37'
+color14='#4fcdb7'
+color15='#fef4f1'
